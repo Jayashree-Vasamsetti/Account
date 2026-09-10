15c22a36f1a5ed34583d800c097221eaccf122f2
Jayashree-Vasamsetti <vasamsettijayashree@gmail.com>
2026-09-10 17:32:58 +0530

    Repository

---
+++ accounts/src/main/java/com/gayatri/accounts/entity/BaseEntity.java
@@ -0,0 +1,35 @@
+package com.gayatri.accounts.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass // it acts as a super class to all the entities  without requiring to create a separate table for it in the database
+@Getter @Setter @ToString
+public class BaseEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @CreatedBy
+    @Column(updatable = false)
+    private String createdBy;
+
+    @LastModifiedDate
+    @Column(insertable = false)
+    private LocalDateTime updatedAt;
+
+    @LastModifiedBy
+    @Column(insertable = false)
+    private String updatedBy;
+}
